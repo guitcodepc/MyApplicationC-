14cf58249dc2cc07633737e76b5bccd064ad8e1e
Guilherme <corporativogui@outlook.com>
2025-09-14 21:27:16 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaAplicacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+            Console.WriteLine(10+10);
+            Console.ReadLine();
+
+            
+        }
+    }
+}
